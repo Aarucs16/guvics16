69e8eb9b11c2c14273081de2eb71472806b43462
Aarucs16 <43890527+Aarucs16@users.noreply.github.com>
2018-10-05 23:31:42 +0530

    Add files via upload

---
+++ checkpos.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class abc
+{
+public static void main(String[] args)
+{
+int n;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+if(n>0)
+System.out.print("Positive");
+else if(n<0)
+System.out.print("Negative");
+else
+System.out.print("Zero");
+}
+}
